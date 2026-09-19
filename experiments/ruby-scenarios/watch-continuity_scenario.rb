c2148d499e279786d89849514d057aff6570b6c4
Laura <laurajoyhutchins@gmail.com>
2026-09-18 23:04:23 -0600

    experiments: remove Ruby scenario layer

--- experiments/ruby-scenarios/watch-continuity_scenario.rb
+++
@@ -1,9 +0,0 @@
-scenario "WATCH continuity decides whether absence evidence survives" do
-  provider :kubernetes_configmap, :kubernetes do
-    continuity :broken, as: :broken_watch
-    continuity :maintained, as: :continuous_watch
-  end
-
-  expect_evidence_preserved :broken_watch, false
-  expect_evidence_preserved :continuous_watch, true
-end
